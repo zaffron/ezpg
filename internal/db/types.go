a3e58c5c5c3b03da24cb6c4df823e37f20cfe441
Avinash Rijal <avinash@oozou.com>
2026-02-09 21:32:57 +0700

    feat: prepare query methods and types

--- internal/db/types.go
+++ internal/db/types.go
@@ -7,8 +7,38 @@ import (
 	"github.com/zaffron/ezpg/internal/config"
 )
 
+/**
+* =============================================================================
+* Manager Related Types
+* =============================================================================
+ */
 type Manager struct {
 	mu    sync.RWMutex
 	pools map[string]*pgxpool.Pool
 	conns map[string]*config.Connection
+}
+
+/**
+* =============================================================================
+* Query Related Types
+* =============================================================================
+ */
+type TableInfo struct {
+	Schema string
+	Name   string
+}
+
+type ColumnInfo struct {
+	Name       string
+	DataType   string
+	IsNullable bool
+	IsPrimary  bool
+}
+
+func (t TableInfo) FullName() string {
+	if t.Schema == "public" {
+		return t.Name
+	}
+
+	return t.Schema + "." + t.Name
 }
